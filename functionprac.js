94d694ecfb9fd56172da45395ae8b3a8802dd6b3
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-03-23 20:06:39 +0530

    save

--- functionprac.js
+++ functionprac.js
@@ -8,6 +8,7 @@ const fun7 =document.querySelector(".class7")
 const fun8 =document.querySelector(".class8")
 const fun9 =document.querySelector(".class9")
 const fun10 =document.querySelector(".class10")
+const fun11 = document.querySelector(".cl1")
 
 function changetouppercase(){
     var data = fun1.innerHTML.toLocaleUpperCase()
@@ -23,6 +24,27 @@ function cleartext() {
      fun3.innerHTML =''
 }
 function copytext() {
-    
-    console.log('copied')
+console.log('copied')
 }
+function arrayname() {
+    var fun12 = document.querySelector(".cl2")
+const producname = ["oil", "shampoo", "paste", "orage" , "egg", "grapes", "red", "blue"]
+console.log(producname[2])
+
+
+
+
+}
+
+//call stack
+
+const multiply = (x, y) => x * y;
+const square = x => multiply(x, x);
+const isRightTriangle = (a, b, c) => (
+    square(a) + square(b) === square(c)
+)
+
+result = isRightTriangle(3, 4, 5)
+console.log(result)
+cls = document.querySelector(".cl2")
+cls.innerHTML=result
