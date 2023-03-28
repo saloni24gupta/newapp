11ceac3a9dbe3168dfa7b266aacf1a32eff2b86b
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-03-28 22:11:26 +0530

    iteratemethod

--- classlistprac.js
+++ classlistprac.js
@@ -12,4 +12,19 @@ for(let  li of lii){
 //         li.classList.add('highlight');
 //     }
 // }
+
+const division = document.querySelector('#container')
+division.style.textAlign = "center";
+const img = document.querySelector("img");
+img.style.width="150px";
+img.style.borderRadius = "50%"
     
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+let i=0;
+const allspans = document.querySelectorAll('h1 span');
+for(let span of allspans) {
+    span.style.color=colors[i]
+    i++;
+}
+
+
