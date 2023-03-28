e317df57ac99af98abf1f44bf0f562969bd98e69
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-03-28 21:49:29 +0530

    save

--- classlistprac.js
+++ classlistprac.js
@@ -2,3 +2,14 @@ const lii = document.querySelectorAll("li")
 for(let  li of lii){
     li.classList.toggle('highlight');
 } 
+
+// for(let li of lii) {
+//     if (li.classList.contains('highlight')
+//     ){
+// li.classList.remove('highlight')
+//     }
+//     else {
+//         li.classList.add('highlight');
+//     }
+// }
+    
