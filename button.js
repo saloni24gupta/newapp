9e75960856d5ef1742d64d1662dc86a58151f963
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-03-28 20:30:22 +0530

    save

--- button.js
+++ button.js
@@ -1,17 +1,17 @@
-const button = document.querySelector("#container")
-for(i=0; i<100; i++) {
+const buttons = document.querySelector("#container")
+for (i = 0; i < 100; i++) {
     const button = document.createElement('button')
     button.innerText = 'button'
-    container.appendChild(button)
+    buttons.appendChild(button)
 }
 
 const box = document.querySelector("#box")
-for(i=0; i<100; i++){
+for (i = 0; i < 100; i++) {
 
     const boxes = document.createElement("box")
     boxes.innerText = 'i am a box';
-   box.appendChild(boxes)
-   boxes.setAttribute('class', 'mb')
+    box.appendChild(boxes)
+    boxes.setAttribute('class', 'mb')
 
 }
 
