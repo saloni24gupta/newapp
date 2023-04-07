99b22ede27fef8b20562275c4f821114c8a1b807
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-04-07 22:48:27 +0530

    ss

--- newpro/src/components/ExpenseItem.jsx
+++ newpro/src/components/ExpenseItem.jsx
@@ -8,8 +8,8 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-            <h2 >${props.Title}</h2>
-            <div className='expense-item__price'>{props.Amount}</div>
+            <h2 >{props.Title}</h2>
+            <div className='expense-item__price'>${props.Amount}</div>
         </div>
     </Card> 
   )
