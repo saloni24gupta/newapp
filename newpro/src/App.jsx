f524d9bd331684bb417e249fced2b3d0c5671a23
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-04-08 16:56:18 +0530

    usestate is a variable that is use to change the state

--- newpro/src/App.jsx
+++ newpro/src/App.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 import Expenses from './components/Expenses.jsx';
 function App() {
 
+//   return (
+//     React.createElement('div', {}, React.createElement('h2', {}, 'Lets get Started'), React.createElement(Expenses))
+// );
+
+
   return (
-    React.createElement('div', {}, React.createElement('h2', {}, 'Lets get Started'), React.createElement(Expenses))
-);
+    <div className='container'>
+      <h1>Welcome to my app</h1>
 
+        <Expenses />
 
-  // return (
-  //   <div className='container'>
-  //     <h1>Welcome to my app</h1>
-
-  //       <Expenses />
-
-  //   </div>
-  // );
+    </div>
+  );
 }
 
 export default App
--- newpro/src/components/ExpenseItem.jsx
+++ newpro/src/components/ExpenseItem.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
 function ExpenseItem(props) {
-
+ 
+const [title, setTitle] = useState(props.Title)
+const clickHandler = () =>  {
+  setTitle('updated');
+console.log(title);
+}
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-            <h2 >{props.Title}</h2>
+            <h2 >{title}</h2>
             <div className='expense-item__price'>${props.Amount}</div>
         </div>
+        <button onClick={clickHandler}>Change Tile</button>
     </Card> 
   )
 }
