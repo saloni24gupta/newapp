22e75e45e5ea5c9d898349855030154a9e800e63
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-04-08 11:04:48 +0530

    usingReactcreateElementmentgod

--- newpro/src/App.jsx
+++ newpro/src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
+import './App.css';
+import React from 'react';
 import Expenses from './components/Expenses.jsx';
 function App() {
-  
+
   return (
-    <div className='container'>
-      <h1>Welcome to my app</h1>
-    
-        <Expenses />
-        
-    </div>
-  );
+    React.createElement('div', {}, React.createElement('h2', {}, 'Lets get Started'), React.createElement(Expenses))
+);
+
+
+  // return (
+  //   <div className='container'>
+  //     <h1>Welcome to my app</h1>
+
+  //       <Expenses />
+
+  //   </div>
+  // );
 }
 
 export default App
