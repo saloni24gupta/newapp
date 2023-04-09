3651d528d970e08a0291a753c73b1d194aa50d0b
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-04-09 10:02:58 +0530

    multipleusestate

--- newpro/src/components/NewExpense/ExpenseForm.jsx
+++ newpro/src/components/NewExpense/ExpenseForm.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './ExpenseForm.css'
 function ExpenseForm() {
+   const [enterTitle, setEnterTitle]  = useState('')
+   const [enterAmount, setEnterAmount] = useState('')
+   const [enterDate, setEnterDate] = useState('')
     const titleChangeHandler = (event) => {
-    console.log(event.target.value)
+    setEnterTitle(event.target.value)
+    }
+    const AmountChangeHandler = (event) => {
+setEnterAmount(event.target.value)
+    }
+    const DateChangeHandler = (event) => {
+setEnterAmount(event.target.value)
     }
     return (
 
@@ -14,11 +23,11 @@ function ExpenseForm() {
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" min="0.01" step="0.01" onChange={AmountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" />
+                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={DateChangeHandler} />
                 </div>
             </div>
             <div className='new-expense__actions' >
