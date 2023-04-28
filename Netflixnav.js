6d84da70ccef4bb249011fdd63be3b9d821ac03e
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-04-28 22:35:37 +0530

    continue

---
+++ Netflixnav.js
@@ -0,0 +1,8 @@
+const open_btn = document.querySelector('.open-btn')
+const close_btn = document.querySelector('.close-btn')
+const nav = document.querySelectorAll('.nav')
+
+open_btn.addEventListener('click', () => {
+    nav.forEach()
+}
+)
