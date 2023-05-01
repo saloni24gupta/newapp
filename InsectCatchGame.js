dd7f61eb00a9988076edb877f241d7c26fee1bd2
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2023-05-01 11:00:44 +0530

    continue

--- InsectCatchGame.js
+++ InsectCatchGame.js
@@ -1,2 +1,27 @@
-const screens = document.querySelectorAll('.screen')
-screens[0].classList.add('up')
+const screens = document.querySelectorAll('.screen');
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn');
+const start_btn = document.getElementById('start-btn');
+const timeEl = document.getElementById('time');
+const scoreEl = document.getElementById('message');
+const game_container = document.getElementById('.game-container');
+
+let seconds = 0
+let score = 0
+let selected_insect = {}
+
+start_btn.addEventListener('click', () => screens[0].classList.add('up'))
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img')
+        const src = img.getAttribute('src')
+        const alt = img.getAttribute('alt')
+        selected_insect = { src, alt }
+        screens[1].classList.add('up')
+        setTimeout(createInsect, 1000)
+        startGame()
+    })
+})
+
+function createInsect() {
+    const insect = document.crea
+}
